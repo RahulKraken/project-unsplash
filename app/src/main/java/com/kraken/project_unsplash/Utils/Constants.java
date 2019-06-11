16210ffafa8d84dd39df43d7c1138d56504adcb8
RahulKraken <r.rahul4511@gmail.com>
2019-06-11 14:28:16 +0530

    added internet permission

--- app/src/main/java/com/kraken/project_unsplash/Utils/Constants.java
+++ app/src/main/java/com/kraken/project_unsplash/Utils/Constants.java
@@ -3,8 +3,13 @@ package com.kraken.project_unsplash.Utils;
 public class Constants {
 
     private final String ACCESS_KEY = "15dbbf8531d6a54d5325124fe9a0f21b897aa7b27b09c982d01cf3b5dd92256c";
+    private final String BASE_URL = "https://api.unsplash.com/";
 
     public String getAccessKey() {
         return ACCESS_KEY;
+    }
+
+    public String getBaseUrl() {
+        return BASE_URL;
     }
 }
