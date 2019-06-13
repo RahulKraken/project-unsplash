b3ae1f186407a3de8b189e8e576066cad5aa496b
RahulKraken <r.rahul4511@gmail.com>
2019-06-13 22:48:15 +0530

    added apply btn and wallpaper changing functionality implemented

--- app/src/main/java/com/kraken/project_unsplash/Activities/ImageViewer.java
+++ app/src/main/java/com/kraken/project_unsplash/Activities/ImageViewer.java
@@ -1,53 +1,101 @@
 package com.kraken.project_unsplash.Activities;
 
+import android.app.WallpaperManager;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.ImageRequest;
 import com.bumptech.glide.Glide;
 import com.kraken.project_unsplash.Models.Photo;
+import com.kraken.project_unsplash.MyApplication;
 import com.kraken.project_unsplash.R;
 
 public class ImageViewer extends AppCompatActivity {
 
+    private static final String TAG = "ImageViewer";
+
     private ImageView imageView;
-    private TextView textView;
+    private TextView tvUserName, tvDescription;
+    private Button setWallpaperBtn;
+    private Photo photo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        hideStatusBar();
         setContentView(R.layout.activity_image_viewer);
 
         imageView = findViewById(R.id.img_viewer);
-        textView = findViewById(R.id.tv_user_name);
+        tvUserName = findViewById(R.id.tv_user_name);
+        tvDescription = findViewById(R.id.tv_description);
+        setWallpaperBtn = findViewById(R.id.setWallpaperBtn);
+
+        Intent intent = getIntent();
+        photo = (Photo) intent.getSerializableExtra(getResources().getString(R.string.photo_intent_transfer_key));
 
         LoadImage();
+        initSetWallpaperBtn();
+    }
+
+    private void initSetWallpaperBtn() {
+        setWallpaperBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ImageRequest imageRequest = new ImageRequest(photo.getLinks().getDownload(), new Response.Listener<Bitmap>() {
+                    @Override
+                    public void onResponse(Bitmap response) {
+                        Log.d(TAG, "onResponse: 200 OK\n" + response.toString());
+                        WallpaperManager wallpaperManager = WallpaperManager.getInstance(getApplicationContext());
+                        try {
+                            wallpaperManager.setBitmap(response);
+                            Toast.makeText(ImageViewer.this, "Wallpaper set successfully!", Toast.LENGTH_SHORT).show();
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                            Toast.makeText(ImageViewer.this, "Failed to set wallpaper!", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                }, 0, 0, ImageView.ScaleType.FIT_CENTER,  null,
+                        new Response.ErrorListener() {
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+                                Log.d(TAG, "onErrorResponse: " + error.getMessage());
+                            }
+                        });
+                MyApplication.getLocalRequestQueue().add(imageRequest);
+            }
+        });
     }
 
     private void LoadImage() {
-        Intent intent = getIntent();
-        Photo photo = (Photo) intent.getSerializableExtra(getResources().getString(R.string.photo_intent_transfer_key));
-
         // load image with glide
         Glide.with(this)
                 .load(photo.getUrls().getRegular())
                 .into(imageView);
 
-        textView.setText(photo.getUser().getName());
+        tvUserName.setText(photo.getUser().getName());
+        if (photo.getDescription() != null || photo.getAlt_description() != null) {
+            tvDescription.setText(photo.getDescription() != null ? photo.getDescription() : photo.getAlt_description());
+        }
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        // hide the status bar
-        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN);
+        hideStatusBar();
     }
 
-    @Override
-    public void onWindowFocusChanged(boolean hasFocus) {
-        super.onWindowFocusChanged(hasFocus);
+    private void hideStatusBar() {
+        // hide the status bar
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN);
     }
 }
