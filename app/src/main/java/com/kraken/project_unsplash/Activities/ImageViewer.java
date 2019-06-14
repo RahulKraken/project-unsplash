fbb599a7ae68a39f84ee3bb126a2ab049ac2afc9
RahulKraken <r.rahul4511@gmail.com>
2019-06-15 02:04:19 +0530

    add to favorites button connected to database

--- app/src/main/java/com/kraken/project_unsplash/Activities/ImageViewer.java
+++ app/src/main/java/com/kraken/project_unsplash/Activities/ImageViewer.java
@@ -2,7 +2,9 @@ package com.kraken.project_unsplash.Activities;
 
 import android.annotation.SuppressLint;
 import android.app.WallpaperManager;
+import android.content.ContentValues;
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -18,26 +20,37 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.ImageRequest;
 import com.bumptech.glide.Glide;
+import com.kraken.project_unsplash.Database.DatabaseContract;
+import com.kraken.project_unsplash.Database.DatabaseHelper;
 import com.kraken.project_unsplash.Models.Photo;
 import com.kraken.project_unsplash.MyApplication;
 import com.kraken.project_unsplash.R;
-
+a
+/**
+ * ImageViewer Activity class
+ */
 public class ImageViewer extends AppCompatActivity {
 
     private static final String TAG = "ImageViewer";
 
+    // widgets
     private ImageView imageView;
     private TextView tvUserName, tvDescription, tvLikesCnt;
     private Button setWallpaperBtn;
     private ImageButton addFavoritesBtn;
+
+    // photo object
     private Photo photo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // hide status bar
         hideStatusBar();
         setContentView(R.layout.activity_image_viewer);
 
+        // refer to the widgets
         imageView = findViewById(R.id.img_viewer);
         tvUserName = findViewById(R.id.tv_user_name);
         tvDescription = findViewById(R.id.tv_description);
@@ -45,34 +58,61 @@ public class ImageViewer extends AppCompatActivity {
         addFavoritesBtn = findViewById(R.id.img_btn_add_favorites);
         tvLikesCnt = findViewById(R.id.tv_likes_count);
 
+        // get the intent from invoking activity
         Intent intent = getIntent();
+        // extract the stored photo with the intent
         photo = (Photo) intent.getSerializableExtra(getResources().getString(R.string.photo_intent_transfer_key));
 
+        // load image into the image view
         LoadImage();
+        // activate the set wallpaper button
         initSetWallpaperBtn();
+        // activate the add to favorites button
         initAddToFavoritesBtn();
     }
 
+    /**
+     * adds current photo id to the fav_table in database
+     */
     private void initAddToFavoritesBtn() {
+        // add onClickListener on addToFavoritesBtn
         addFavoritesBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Log.d(TAG, "onClick: add to favorites " + photo.getId());
-                Toast.makeText(ImageViewer.this, "Adding to favorites", Toast.LENGTH_SHORT).show();
+
+                // get the database
+                DatabaseHelper helper = new DatabaseHelper(ImageViewer.this);
+                SQLiteDatabase database = helper.getWritableDatabase();
+
+                // create the content values
+                ContentValues values = new ContentValues();
+                values.put(DatabaseContract.FavoritesEntry.COLUMN_PHOTO_ID, photo.getId());
+
+                // insert into the database
+                database.insert(DatabaseContract.FavoritesEntry.TABLE_NAME, null, values);
+                database.close();
             }
         });
     }
 
+    /**
+     * download the image and set it as wallpaper
+     */
     private void initSetWallpaperBtn() {
+        // add onClickListener to set wallpaper btn
         setWallpaperBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                // image request to download image
                 ImageRequest imageRequest = new ImageRequest(photo.getLinks().getDownload(), new Response.Listener<Bitmap>() {
                     @Override
                     public void onResponse(Bitmap response) {
                         Log.d(TAG, "onResponse: 200 OK\n" + response.toString());
+                        // wallpaper manager instance
                         WallpaperManager wallpaperManager = WallpaperManager.getInstance(getApplicationContext());
                         try {
+                            // set downloaded bitmap as wallpaper
                             wallpaperManager.setBitmap(response);
                             Toast.makeText(ImageViewer.this, "Wallpaper set successfully!", Toast.LENGTH_SHORT).show();
                         } catch (Exception e) {
@@ -92,6 +132,9 @@ public class ImageViewer extends AppCompatActivity {
         });
     }
 
+    /**
+     * load image into image view
+     */
     @SuppressLint("SetTextI18n")
     private void LoadImage() {
         // load image with glide
@@ -99,6 +142,7 @@ public class ImageViewer extends AppCompatActivity {
                 .load(photo.getUrls().getRegular())
                 .into(imageView);
 
+        // set text in the text fields
         tvUserName.setText(photo.getUser().getName());
         if (photo.getDescription() != null || photo.getAlt_description() != null) {
             tvDescription.setText(photo.getDescription() != null ? photo.getDescription() : photo.getAlt_description());
@@ -109,9 +153,13 @@ public class ImageViewer extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
+        // hide status bar
         hideStatusBar();
     }
 
+    /**
+     * hides the status bar
+     */
     private void hideStatusBar() {
         // hide the status bar
         getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN);
